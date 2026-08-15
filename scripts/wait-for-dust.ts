554e89261bbba309241e06adfbdfbfd5f929108b
Gyan <gyanprakasht987@gmail.com>
2026-08-15 19:33:36 +0530

    Update wait-for-dust.ts

--- scripts/wait-for-dust.ts
+++ scripts/wait-for-dust.ts
@@ -1,6 +1,6 @@
 // This file is part of example-hello-world.
 // Copyright (C) Midnight Foundation
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
